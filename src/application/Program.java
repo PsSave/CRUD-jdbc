f88c5d3c4832ec9d8509d27ad7784f616749c9c2
Pedro Samuel <pedrosviki1265@gmail.com>
2023-03-29 14:10:22 -0400

    Adicionado upade e delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,9 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
 		SellerDao sellerDao = new DaoFactory().createSellerDao();
 		
 		System.out.println("=== TESTE 1: findById ===");
@@ -36,6 +40,21 @@ public class Program {
 		System.out.println("=== TESTE 4: insert ===");
 		Seller newSeller = new Seller(null, "Gregorio", "gregorio@gmail.com", new Date(), 4000.00, department);
 		
+		System.out.println();
+		System.out.println("=== TESTE 5: update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Vitoria Regina");
+		sellerDao.update(seller);
+		System.out.println("Update completo");
+		
+		System.out.println();
+		System.out.println("=== TESTE 6: delete ===");
+		System.out.println("Digite o Id para ser deletado: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Deletado!");
+		
+		sc.close();
 	}
 
 }
